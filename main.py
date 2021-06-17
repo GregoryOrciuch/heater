e2ff9b2fb4ace8efd9fdf6d8f39ff4acb4b450c8
Gregory Orciuch <grzegorz.orciuch@localised.com>
2021-06-17 10:07:44 +0200

    init2

--- main.py
+++ main.py
@@ -107,7 +107,54 @@ def test_procedure():
     print("test is finished")
 
 
+def operation():
+    temp = get_temp()
+    print("t: " + str(temp))
+    voltage = get_voltage()
+    print("v: " + str(voltage))
+
+    # if v>28.5 and v< 28.9, turn on the heater for next 30min
+    try:
+        state = read_state()
+        if 'turn-on-time' in state:
+            turn_on_time = datetime.fromisoformat(state['turn-on-time'])
+            print("Last turn on time was: "+turn_on_time.isoformat())
+            minutes_diff = (datetime.now() - turn_on_time).total_seconds() / 60.0
+            if minutes_diff > HEATER_MAX_RUN_MIN:
+                print("Heating time has passed")
+                turn_off()
+            else:
+                print("Doing nothing, should remain ON")
+                turn_on()
+        else:
+            print("Turn on time was not found")
+
+        if 'turn-off-time' in state:
+            turn_off_time = datetime.fromisoformat(state['turn-off-time'])
+            print("Last turn off time was: " + turn_off_time.isoformat())
+            minutes_diff = (datetime.now() - turn_off_time).total_seconds() / 60.0
+            if minutes_diff > HEATER_MAX_COOLDOWN_MIN:
+                # turn on if voltage is good
+                if 28.5 < voltage < 28.9:
+                    print("heater was cooled down, voltage is good, turning on")
+                    turn_on()
+                else:
+                    print("voltage not in desired range")
+            else:
+                print("Cooldown time not met, doing nothing.")
+
+        else:
+            print("Turn off time was not found")
+
+    except FileNotFoundError as e:
+        print("Heater state was not found")
+
+
+
 if __name__ == '__main__':
 
     HEATER_IP = "192.168.0.118"
-    test_procedure()
+    HEATER_MAX_RUN_MIN = 5
+    HEATER_MAX_COOLDOWN_MIN = 1
+    #test_procedure()
+    operation()
