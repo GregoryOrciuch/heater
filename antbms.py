cc05887a4bf9fbcebd6053ab06ef1ddea748c7d7
Gregory Orciuch <grzegorz.orciuch@localised.com>
2021-09-03 13:34:20 +0200

    fdfffddd

--- antbms.py
+++ antbms.py
@@ -185,6 +185,10 @@ if __name__ == "__main__":
             cell_max = struct.unpack('>H', resp[116:118])[0]*0.001
             mqttc.publish("bms/cell_max", cell_max)
 
+            print("resp:"+resp[103:105])
+            fet_ch_st = str(int(struct.unpack('>h', resp[103:105])[0],16))
+            mqttc.publish("bms/fet_ch_st", fet_ch_st)
+
             time.sleep(1)
 
     if ser.isOpen():
